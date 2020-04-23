b3fd26fb22f6bfaaae9d2f5f861f6400ac37cdff
Nikesh <nikeshwarpandey@gmail.com>
2020-04-23 22:21:53 +0530

    Refinement Of Code,Removing consol.log And Replacing let with const as per requirement.

--- js-exercises/is-triangle/isTriangle.js
+++ js-exercises/is-triangle/isTriangle.js
@@ -1,15 +1,10 @@
 function isTriangle(sideA, sideB, sideC) {
-  let sumofAB = sideA + sideB;
-  let sumofBC = sideB + sideC;
-  let sumofCA = sideC + sideA;
-  if (sumofAB > sideC && sumofBC > sideA && sumofCA > sideB) {
-    return true
-  } else {
-    return false
-  }
+  const sumofAB = sideA + sideB;
+  const sumofBC = sideB + sideC;
+  const sumofCA = sideC + sideA;
+  return (sumofAB > sideC && sumofBC > sideA && sumofCA > sideB)
 }
-// console.log(isTriangle(3, 4, 5));
-// console.log(isTriangle(1, 4, 2));
+
 export {
   isTriangle,
 };
